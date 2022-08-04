f3ff3854d6d62e904e8ae4332cdc7f1400ebd235
ana-gujabidze <ana.gujabidze@quantori.com>
2022-08-04 21:04:33 +0400

    Update settings

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -31,11 +31,11 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://*', 'https://www.*']
+CSRF_TRUSTED_ORIGINS = ['https://*.heroku.com', 'https://www.*.heroku.com']
 
 # Application definition
 
