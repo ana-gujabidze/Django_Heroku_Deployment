2e2d9671290877345c4c5498a9f2736525a3581a
ana-gujabidze <ana.gujabidze@quantori.com>
2022-08-04 20:44:21 +0400

    Enable whitenoise

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'learning_users.urls'
@@ -139,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ STATIC_DIR, ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
